5a11d880cce2516973d66c1bf0c5ed77644b52a3
Aser Kroma <asser.krouma232@gmail.com>
2023-04-07 17:27:10 +0000

    Edited the Main class to have a small sample of runnable code, edited runjar.cmd and pom.xml to include algs4 library as dependency so end user can create and run the jar with that library.

--- src/main/java/hi/HBV202G/Main.java
+++ src/main/java/hi/HBV202G/Main.java
@@ -1,7 +1,35 @@
 package hi.HBV202G;
+import edu.princeton.cs.algs4.*;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        In books=new In("file:E:\\HBV\\semester 2\\Hugbunadar\\Projects\\FinalProject\\src\\main\\java\\hi\\HBV202G\\Booklist.txt");
+        while (books.hasNextLine()){
+            String [] words =books.readLine().split(",");
+            for(int i=0; i<words.length; i++){
+                System.out.println(words[i]);
+            }
+        }
+        while (scanner.hasNext()){
+            System.out.println();
+        }
+        System.out.println("Are you an administrator or a student?");
+
+            switch (scanner.next()) {
+                case "admin":
+                    System.out.println("Hello admin");
+
+                case "student":
+                    System.out.println("Hello student");
+
+                case "end":
+                    break;
+
+        }
     }
+
+
 }
