71e1bc886be003066a8f61a7d661268f0e54aa2f
Aser Kroma <asser.krouma232@gmail.com>
2023-04-21 09:45:49 +0000

    Fixed runjar, so it runs and the pomxml's project description

--- runjar.cmd
+++ runjar.cmd
@@ -1,1 +1,1 @@
-java -cp target/FinalProject-1.0-SNAPSHOT.jar;target/dependency/algs4-1.0.0.0.jar hi.HBV202G.LibrarySystemApplication
+java -cp "target/FinalProject-1.0-SNAPSHOT.jar;target/dependency/algs4-1.0.0.0.jar" hi.HBV202G.LibrarySystemApplication
